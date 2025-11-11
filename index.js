06de6d7057402e4d2d6c45b0e645e4442b9d890e
Tanvir-programmer <tanvir.webprogrammer@gmail.com>
2025-11-11 08:05:51 +0300

    global variable created!

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-// 1. Load environment variables first
-require("dotenv").config(); // Reads .env and adds variables to process.env
-
+require("dotenv").config({ path: "./.env.local" });
+const cors = require("cors");
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
 // 2. Read the URI from the environment variables (process.env)
 const uri = process.env.MONGODB_URI;
 
 // Check if URI is loaded (a good practice)
 if (!uri) {
-  console.error("❌ MONGODB_URI not found in environment variables. Check your .env file!");
+  console.error(
+    "❌ MONGODB_URI not found in environment variables. Check your .env file!"
+  );
   process.exit(1);
 }
 
@@ -27,25 +30,26 @@ const client = new MongoClient(uri, {
 async function connectToMongoDB() {
   try {
     await client.connect();
-    
+
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "✅ MongoDB: Ping successful. Connected to the database!"
-    );
-    
+    console.log("✅ MongoDB: Ping successful. Connected to the database!");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
-    process.exit(1); 
+    process.exit(1);
   }
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello World! Server is running.");
+const db = client.db("freelio");
+const usersCollection = db.collection("job");
+
+app.get("/alljobs", (req, res) => {
+  
+  res.send(usersCollection.find().toArray());
+
 });
 
-connectToMongoDB()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`🚀 Express server listening on port ${port}`);
-    });
+connectToMongoDB().then(() => {
+  app.listen(port, () => {
+    console.log(`🚀 Express server listening on port ${port}`);
   });
+});
